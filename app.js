bedc016329aa3a80feefd3df02beee2cf3c6bc99
Rowena Chen <rwnchen@gmail.com>
2019-11-14 12:02:39 -0500

    actually db set up

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const layout = require("./views/layout.js")
+const layout = require("./views/layout.js");
+const Sequelize = require("sequelize");
+
+const { db, Page, User } = require("./models");
 
 const app = express();
+const PORT = 7879;
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res, next) => {
-  res.send(layout(''));
-})
+app.get("/", (req, res, next) => {
+  res.send(layout(""));
+});
 
+const init = async () => {
+  await db.sync({ force: true });
 
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+};
 
-const PORT = 7879;
-
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-})
+init();
